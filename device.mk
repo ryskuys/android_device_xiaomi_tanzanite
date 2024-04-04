8c7d03d42c75c685755c5f40d700b52c39481f4a
Matsvei Niaverau <matvej2002@gmail.com>
2024-04-04 10:39:01 +0200

    tanzanite: Build FMRadio
    * MtkFMRadio is no longer needed, original one was updated for Mediatek support
    
    Change-Id: I4e0ebd7352ad04e97ce65d3aec5a1b9d030fb3c3
    Signed-off-by: Nathan Andrew <nathannxx@proton.me>

--- device.mk
+++ device.mk
@@ -151,6 +151,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# FM Radio
+PRODUCT_PACKAGES += \
+    FMRadio
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
