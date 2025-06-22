90e45c6b46fef1d1388cdd9a8e25d1e25b86c96d
claxten10 <claxten10@gmail.com>
2025-06-22 10:14:24 +0000

    tanzanite: Move to common MediaTek IMS
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -147,14 +147,6 @@ blob_fixups: blob_fixups_user_type = {
     .add_needed('libssl-v32.so'),
     'vendor/lib64/mt6789/libmnl.so': blob_fixup()
     .add_needed('libcutils.so'),
-    'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
-    .apktool_patch('blob-patches/ImsService.patch', '-r'),
-    'system_ext/lib64/libimsma.so': blob_fixup()
-    .replace_needed('libsink.so', 'libsink-mtk.so'),
-    'system_ext/lib64/libsink-mtk.so': blob_fixup()
-    .add_needed('libaudioclient_shim.so'),
-    'system_ext/lib64/libsource.so': blob_fixup()
-    .add_needed('libui_shim.so'),
     'vendor/lib64/libdlbdsservice.so': blob_fixup()
     .replace_needed("libstagefright_foundation.so", "libstagefright_foundation-v33.so"),
     'vendor/lib64/mt6789/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
