418eba77449f96359fd6380e3d07df1ea69db586
xyzuniverse <dev.xyzuniverse@gmail.com>
2025-07-22 07:14:06 +0200

    tanzanite: Update patches from OS2.0.202.0.VOGEUXM
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -147,7 +147,7 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/mt6789/libmnl.so': blob_fixup()
     .add_needed('libcutils.so'),
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
-    .apktool_patch('blob-patches/ImsService.patch', '-r'),
+    .apktool_patch('blob-patches/ImsService.patch'),
     'system_ext/lib64/libimsma.so': blob_fixup()
     .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system_ext/lib64/libsink-mtk.so': blob_fixup()
