1198c555058f1f00f098803d7491584f2b315129
Nathan Andrew <nathannxx@proton.me>
2025-07-26 23:57:23 +0000

    tanzanite: add `ViPER4Android FX`
    Signed-off-by: Nathan Andrew <nathannxx@proton.me>

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 # Keys
 $(call inherit-product-if-exists, vendor/private/keys/keys.mk)
 
+# ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # AB OTA Configuration
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS := \
