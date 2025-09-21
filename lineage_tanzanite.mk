524ec419e9cdf8f992528f612975caf268a75a89
Nathan Andrew <nathannxx@proton.me>
2025-09-21 00:12:26 +0000

    tanzanite: uprev blobs to `OS2.0.205.0.VOGEUXM`
    Signed-off-by: Nathan Andrew <nathannxx@proton.me>

--- lineage_tanzanite.mk
+++ lineage_tanzanite.mk
@@ -25,5 +25,5 @@ TARGET_ENABLE_BLUR := true
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="missi-user 15 AP3A.240905.015.A2 OS2.0.202.0.VOGEUXM release-keys" \
-    BuildFingerprint=Redmi/tanzanite_n_global/tanzanite:15/AP3A.240905.015.A2/OS2.0.202.0.VOGEUXM:user/release-keys
+    BuildDesc="missi-user 15 AP3A.240905.015.A2 OS2.0.205.0.VOGEUXM release-keys" \
+    BuildFingerprint=Redmi/tanzanite_n_global/tanzanite:15/AP3A.240905.015.A2/OS2.0.205.0.VOGEUXM:user/release-keys
